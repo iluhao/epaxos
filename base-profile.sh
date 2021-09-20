32bd70e9062942ae466393f8b758b85c1ea2744e
Jesse Tuglu <jktuglu@gmail.com>
2021-09-19 23:27:23 -0400

    add both clock times

--- base-profile.sh
+++ base-profile.sh
@@ -1,10 +1,10 @@
-ServerIps=(10.10.1.1 10.10.1.3 10.10.1.5) # 3
-ClientIps=(10.10.1.2)
+ServerIps=(10.10.1.1 10.10.1.2 10.10.1.3) # 3
+ClientIps=(10.10.1.4)
 MasterIp=10.10.1.1
 FirstServerPort=17070 # change it when only necessary (i.e., firewall blocking, port in use)
 NumOfServerInstances=3 # before recompiling, try no more than 5 servers. See Known Issue # 4
-NumOfClientInstances=2 #20,40,60,80,100,200,300,400,500
-reqsNb=20000
+NumOfClientInstances=10 #20,40,60,80,100,200,300,400,500
+reqsNb=60000
 writes=50
 dlog=false
 conflicts=0
